644f2d7f86444edf40b0b0d40788d8ff1694a956
HARSHA VINJAMURI <121868338+HARSHAVINJAMURI@users.noreply.github.com>
2025-02-12 12:01:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import urllib.request as url
 import re
 import nltk
 import heapq
+import os
 from string import punctuation
 # import nltk
 # nltk.download('all')
@@ -76,4 +77,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
